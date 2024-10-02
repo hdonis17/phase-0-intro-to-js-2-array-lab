699cb1ca5ec0e3fc67db7fc5c54f16b624ca57a0
Harrison Donis <hdonis17@gmail.com>
2024-10-02 14:39:12 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats;
+
+cats = [];
+    cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
